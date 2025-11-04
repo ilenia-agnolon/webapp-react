7fdc23af80955e971500c009c550919b18691016
ilemia-agnolon <ilenia.agnolon.96@gmail.com>
2025-11-04 16:50:14 +0100

    - configuarata l’app di backend (webapp-express) a ricevere chiamate dall'applicazione React (webapp-react), installando e impostando il middleware CORS
    - effettuata una chiamata Ajax dalla home del progetto React, per ottenere la lista dei film

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,33 @@
+// import axios
+import axios from "axios";
+
+// import state e effetc
+import { useState, useEffect } from "react";
+
+//componente HomePage
 const HomePage = () => {
+  // stato: lista dei film
+  const [movies, setMovies] = useState([]);
+
+  // funzione che esegue la chiamata AJAX
+  const fetchMovies = () => {
+    axios
+      .get("http://localhost:3000/api/movies")
+      .then((response) => {
+        // response.data = array di film restituito dal backend
+        setMovies(response.data);
+      })
+      .catch((error) => {
+        console.log("Errore nel caricamento film:", error);
+      });
+  };
+
+  // esegue fetchMovies una sola volta al montaggio della pagina
+  useEffect(fetchMovies, []);
+
+  //verifica
+  console.log("Movies:", movies);
+
   return (
     <div className="movies-container">
       {/* MOVIE CARD _____________________ */}
